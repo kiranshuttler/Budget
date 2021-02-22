db041fe11929277cc803010af4edd133d37d4d05
SAIKRISHNA MUDDULURU <muddulurusaikrishna@gmail.com>
2021-02-21 21:14:04 -0600

    test-6 commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var budgetController = (function() {
     
     
     var Income = function(id, description, value) {
-        this.id = id;
+        this.id = id
         this.description = description
         this.value = value
     };
